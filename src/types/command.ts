a4dede58fdcda2ae2bff5e1ac79955bdabe2073f
Deraj <75500118+derajoxford@users.noreply.github.com>
2025-11-15 09:49:30 -0600

    Update command.ts

--- src/types/command.ts
+++ src/types/command.ts
@@ -5,14 +5,23 @@ import {
   ModalSubmitInteraction,
   SlashCommandBuilder,
   SlashCommandSubcommandsOnlyBuilder,
+  SlashCommandOptionsOnlyBuilder,
 } from "discord.js";
 
 export type Command = {
-  data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+  // Allow all three builder shapes so simple commands (no subcommands)
+  // that infer SlashCommandOptionsOnlyBuilder compile cleanly.
+  data:
+    | SlashCommandBuilder
+    | SlashCommandSubcommandsOnlyBuilder
+    | SlashCommandOptionsOnlyBuilder;
+
   execute: (interaction: ChatInputCommandInteraction) => Promise<any>;
   handleModal?: (interaction: ModalSubmitInteraction) => Promise<any>;
+
   // Return true if handled, false to allow other handlers to try
   handleButton?: (i: ButtonInteraction) => Promise<boolean>;
+
   // Registry may check this
   disabled?: boolean;
 };
